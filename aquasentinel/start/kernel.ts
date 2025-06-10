5f2c0f3f74441709b2263d40785da5368bfca4e1
raissa <bombokoraissa@gmail.com>
2025-06-10 07:29:25 +0100

    Update middleware configuration: Move session middleware to server stack and remove from router stack

--- aquasentinel/start/kernel.ts
+++ aquasentinel/start/kernel.ts
@@ -25,7 +25,8 @@ server.errorHandler(() => import('#exceptions/handler'))
 server.use([
   () => import('#middleware/container_bindings_middleware'),
   () => import('@adonisjs/static/static_middleware'),
-  () => import('@adonisjs/vite/vite_middleware')
+  () => import('@adonisjs/vite/vite_middleware'),
+  () => import('@adonisjs/session/session_middleware'),
 ])
 
 /**
@@ -34,7 +35,6 @@ server.use([
  */
 router.use([
   () => import('@adonisjs/core/bodyparser_middleware'),
-  () => import('@adonisjs/session/session_middleware'),
   () => import('@adonisjs/shield/shield_middleware'),
   () => import('@adonisjs/auth/initialize_auth_middleware')
 ])
