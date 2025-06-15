cdcf8c824474b7fe2d617c138e8b6ea27c3c92f4
raissa <bombokoraissa@gmail.com>
2025-06-15 08:35:16 +0100

    feat: update PostgreSQL connection configuration in database.ts

--- aquasentinel/config/database.ts
+++ aquasentinel/config/database.ts
@@ -7,14 +7,18 @@ const dbConfig = defineConfig({
     postgres: {
       client: 'pg',
       connection: env.get('NODE_ENV') === 'production' ? {
-        connectionString: env.get('DATABASE_URL'),
-        ssl: { rejectUnauthorized: false }
+        host: env.get('DB_HOST'),
+            port: Number(env.get('DB_PORT')),
+            user: env.get('DB_USER'),
+            password: env.get('DB_PASSWORD', '') + '', // Ensure password is always a string
+            database: env.get('DB_DATABASE'),
+            ssl: env.get('DB_SSL', 'false') === 'true' ? { rejectUnauthorized: false } : false,
       }
         : {
           host: env.get('DB_HOST'),
           port: env.get('DB_PORT'),
           user: env.get('DB_USER'),
-          password: env.get('DB_PASSWORD'),
+          password: env.get('DB_PASSWORD','') + '',
           database: env.get('DB_DATABASE'),
         },
       migrations: {
